955107d2e6ee233c7dcbbc8a6495742384d07fa2
Denisse Suarez <dsuarez@thoughtworks.com>
2017-07-09 03:06:28 -0500

    First commit

---
+++ src/main/java/com/dijkstra/DijkstraLauncher.java
@@ -0,0 +1,12 @@
+package com.dijkstra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DijkstraLauncher {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DijkstraLauncher.class, args);
+	}
+}
