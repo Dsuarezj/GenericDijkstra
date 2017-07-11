b8d3c0f7f2ef3e27b7b083f11377b4f3276601f7
Denisse Suarez <dsuarez@thoughtworks.com>
2017-07-11 19:43:25 -0400

    Create Node object with th option to set initial and final node

---
+++ src/main/java/com/dijkstra/domain/Node.java
@@ -0,0 +1,44 @@
+package com.dijkstra.domain;
+
+import com.dijkstra.enums.NodeType;
+
+public class Node {
+
+    private String id;
+    private NodeType nodeType;
+    private int totalCost;
+    private String previousNode;
+
+    public Node(String id, NodeType nodeType) {
+        this.id = id;
+        this.nodeType = nodeType;
+        //Otra duda, el objeto debería contener la lógica de poner el costo para le nodo inicial?
+        this.totalCost = nodeType == NodeType.START ? 0 : Integer.MAX_VALUE;
+        this.previousNode = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public NodeType getNodeType() {
+        return nodeType;
+    }
+
+    public int getTotalCost() {
+        return totalCost;
+    }
+
+    public String getPreviousNode() {
+        return previousNode;
+    }
+
+    public Node setAsInitial() {
+        //Primera duda, es mejor mutar el objeto o regresar uno nuevo?
+        return new Node(this.id, NodeType.START);
+    }
+
+    public Node setAsFinal() {
+        return new Node(this.id, NodeType.FINISH);
+    }
+}
---
+++ src/test/java/com/dijkstra/domain/NodeTest.java
@@ -0,0 +1,51 @@
+package com.dijkstra.domain;
+
+import com.dijkstra.enums.NodeType;
+import org.junit.Test;
+
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+public class NodeTest {
+
+    @Test
+    public void shouldCreateANode() {
+        String id = randomAlphabetic(10);
+        int startingNodeCost = Integer.MAX_VALUE;
+
+        Node node = new Node(id, NodeType.NODE);
+
+        assertThat(node.getId(), is(id));
+        assertThat(node.getNodeType(), is(NodeType.NODE));
+        assertThat(node.getTotalCost(), is(startingNodeCost));
+        assertThat(node.getPreviousNode(), is(id));
+    }
+
+    @Test
+    public void shouldSetInitialNode() {
+        String id = randomAlphabetic(10);
+        int cost = 0;
+        Node node = new Node(id, NodeType.NODE);
+
+        Node initialNode = node.setAsInitial();
+
+        assertThat(initialNode.getId(), is(id));
+        assertThat(initialNode.getNodeType(), is(NodeType.START));
+        assertThat(initialNode.getTotalCost(), is(cost));
+        assertThat(initialNode.getPreviousNode(), is(id));
+    }
+
+    @Test
+    public void shouldSetFinalNode() {
+        String id = randomAlphabetic(10);
+        int cost = Integer.MAX_VALUE;
+        Node node = new Node(id, NodeType.NODE);
+
+        Node initialNode = node.setAsFinal();
+
+        assertThat(initialNode.getId(), is(id));
+        assertThat(initialNode.getNodeType(), is(NodeType.FINISH));
+        assertThat(initialNode.getTotalCost(), is(cost));
+        assertThat(initialNode.getPreviousNode(), is(id));
+    }
+}
